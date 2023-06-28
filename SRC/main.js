93d167116798f6337a2ed2a1405e512db2299047
爱吃苦瓜的王某 <89827068+TerminatorForMHT@users.noreply.github.com>
2023-06-29 01:42:35 +0800

    Update main.js

--- SRC/main.js
+++ SRC/main.js
@@ -4,12 +4,18 @@ function createWindow() {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#FFFFFF00',
+      symbolColor: '#74b1be',
+      height: 10
+    },
     webPreferences: {
       nodeIntegration: true
     }
   })
 
-  win.loadFile('../html/new.html')
+  win.loadFile('../html/index.html')
 }
 
 app.whenReady().then(createWindow)
