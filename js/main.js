e42df061cf7577e32902b560782b5e976286631c
wangchenjin <wangchenjin-no.1@outlook.com>
2023-07-03 08:22:41 +0800

    修改主页样式

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
-const {app, BrowserWindow} = require('electron')
-const {platform} = require("os");
+const { app, BrowserWindow } = require('electron')
+const { platform } = require("os");
 
 function createWindow() {
 
     if (process.platform == 'darwin') {
         let mainWindow = new BrowserWindow({
-            width: 800,
-            height: 600,
+            show: false,
             frame: false,
             titleBarStyle: 'customButtonsOnHover',
+            webPreferences: { experimentalFeatures: true },
         })
         mainWindow.loadFile('../examples/simple.html')
-
+        mainWindow.maximize()
+        mainWindow.show()
     } else {
         let mainWindow = new BrowserWindow({
-            width: 800,
-            height: 600,
+            show: false,
             frame: false,
             titleBarStyle: 'hidden',
             titleBarOverlay: {
                 color: '#FFFFFF00',
                 symbolColor: '#666666',
                 height: 15
-            }
+            },
+            webPreferences: { experimentalFeatures: true },
         })
         mainWindow.loadFile('../examples/simple.html')
-
+        mainWindow.maximize()
+        mainWindow.show()
     }
 }
 
